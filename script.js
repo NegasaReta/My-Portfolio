59ef688e81c61302caf8c76112d08a135fe6ca71
NegasaReta <167341232+NegasaReta@users.noreply.github.com>
2025-01-25 18:09:53 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,19 @@
-document.getElementById('contact-form').addEventListener('submit', function(event) {
-    event.preventDefault();
-    alert('Form submitted!');
-});
 
-document.getElementById('contact-form').addEventListener('submit', function(event) {
-    event.preventDefault();
 
-    emailjs.sendForm('service_v1jqynv', 'template_x1i0b4n', this)
-        .then(function() {
-            alert('Message sent successfully!');
-        }, function(error) {
-            alert('Failed to send message. Please try again.');
-        });
-});
+function sendEmail (){
+    let parameters = {
+        name:document.getElementById('name').value,
+        email:document.getElementById('email').value,
+        message:document.getElementById('message').value,
+        subject:document.getElementById('subject').value
+    }
+    emailjs.sendForm('service_v1jqynv', 'template_x1i0b4n', parameters)
+    .then(function() {
+        alert('Message sent successfully!');
+    }, function(error) {
+        alert('Failed to send message. Please try again.');
+    });
 
-function sendEmail() {
-    var form = document.getElementById('contact-form');
-
-    emailjs.sendForm('service_v1jqynv', 'template_x1i0b4n', form)
-        .then(function() {
-            alert('Message sent successfully!');
-        }, function(error) {
-            alert('Failed to send message. Please try again.');
-        });
 }
+
+
